d554bae31d878cacf1588a8da6b08c883c1bc163
Matthias Ulrich <post@ulrich.digital>
2023-03-18 21:22:22 +0100

    Update multisite_ajax_form_ajax.js

--- multisite_ajax_form/multisite_ajax_form_ajax.js
+++ multisite_ajax_form/multisite_ajax_form_ajax.js
@@ -1,15 +1,20 @@
 jQuery(document).ready(function ($) {
 
-    $('#new-facility-request').submit( function (e) {
+    $("form[id*='multipage-ajax-form-']").submit(function (e) {
 
         e.preventDefault();
       
+        $this = $(this);
         // grab content
-        $my_content = $(".summary").html();
+        $my_content = $(this).find(".summary").html();
+
+        // Message-Container after form
+        var my_msg_container = $(this).next(".msg_container");
+        console.log("hey" + my_msg_container);
 
         //  Bedingungen checked?
-        if ($('.acf-field[data-name="bedingungen"] .acf-input label').hasClass('selected')) {
-            $(".msg_failed").css("display", "none");
+        if ($(this).find('.acf-field[data-name="bedingungen"] .acf-input label').hasClass('selected')) {
+            $(my_msg_container).find(".msg_failed").css("display", "none");
 
 
             /*
@@ -32,21 +37,22 @@ jQuery(document).ready(function ($) {
                 success: function (data, textStatus, XMLHttpRequest) {
 
                     // Success Nachricht einblenden, nach 5 Sekunden wieder ausblenden
-                    $(".msg_success").fadeIn("slow").delay(5000).fadeOut("slow");
+                    $(my_msg_container).find(".msg_success").fadeIn("slow").delay(5000).fadeOut("slow");
 
-                    // Akkordion schliessen
-                    $(".multisite_ajax_form .acf-field-accordion.-open").removeClass("-open");
-                    $(".multisite_ajax_form .acf-accordion-content").css("display", "none");
+                    // close accordion 
+                    $this.find(".acf-field-accordion.-open").removeClass("-open");
+                    $this.find(".acf-accordion-content").css("display", "none");
 
-                    // Formular-Navigation ausblenden
-                    $(".multisite_ajax_form .form_prev_next_container").css("display", "none");
+                    // hide form navigation
+                    $this.find(".form_prev_next_container").css("display", "none");
                 },
                 error: function (XMLHttpRequest, textStatus, errorThrown) {
                     console.log(errorThrown);
                 }
             });
         }else{
-            $(".msg_failed").fadeIn("slow").delay(5000).fadeOut("slow");
+            // show failed container
+            $(my_msg_container).find(".msg_failed").fadeIn("slow").delay(5000).fadeOut("slow");
            
         }
     });
