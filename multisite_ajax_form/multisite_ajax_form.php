fe88cb1a60b3be3327c6b12cbf682c0fec89dbfb
Matthias Ulrich <post@ulrich.digital>
2023-03-18 21:21:58 +0100

    Update multisite_ajax_form.php

--- multisite_ajax_form/multisite_ajax_form.php
+++ multisite_ajax_form/multisite_ajax_form.php
@@ -1,4 +1,17 @@
-<?php 
+<?php
+
+/**
+ * Multi-Page-AJAX-Form Template.
+ *
+ * @param   array $block The block settings and attributes.
+ * @param   string $content The block inner HTML (empty).
+ * @param   bool $is_preview True during backend preview render.
+ * @param   int $post_id The post ID the block is rendering content against.
+ *          This is either the post ID currently being displayed inside a query loop,
+ *          or the post ID of the post hosting this block.
+ * @param   array $context The context provided to the block by the post or it's parent block.
+ */
+
 acf_form([
     'field_groups' => ['group_63e6803ee1c1f'],
     'form_attributes' => array("class" => "multisite_ajax_form"),
